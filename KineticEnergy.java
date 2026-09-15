2f4fefdeea07c9add7c0496fc393c1af252ce864
sarahisophia <quinteroval.sarah@student.ccm.edu>
2026-09-15 12:50:12 -0400

    AI REPORT

--- KineticEnergy.java
+++ KineticEnergy.java
@@ -1,3 +1,25 @@
-public class KineticEnergy {
+import java.util.Scanner;
+public class KineticEnergy
+{
+    public static void main(String[] args) 
+    {
+        double mass;
+        double velocity;
+        double k;
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Enter the objects mass (kilograms): ");
+        mass = keyboard.nextDouble();
+        
+        System.out.print("Enter the object's velocity (meters per second): ");
+        velocity = keyboard.nextDouble();
+        
+        k = kineticEnergy(mass, velocity);
+        System.out.printf("The object's kinetic energy is: %,.2f %n", k);
+    }
 
+
+    public static double kineticEnergy(double mass, double velocity) 
+    {
+        return 0.5 * mass * Math.pow(velocity, 2);
+    }
 }
